a08f4ccb438a9f568ea150efe56b0f3572475093
mike <mike@10.208.56.93>
2021-01-16 19:42:21 +0300

    first

---
+++ academy/src/academy/type.java
@@ -0,0 +1,20 @@
+package academy; // Индетификатор пакета
+   public class type{
+public static void main(String... args) {
+	/*  
+	 * ТУТ КОММЕНТАРИЙ 
+	 */
+int Age;
+    Age=22;
+long passportNumber = 931921231;
+char yourMark = 'K';
+double averageMark = 4.12;
+byte test = (byte) 254; // ???
+boolean isActive = false;
+boolean isAlive = true;
+	System.out.println("Your exam results: ");
+	System.out.println("your age is: " + Age + ", passport number: " + passportNumber);
+	System.out.println("you got " + yourMark + ", average mark is " + averageMark);
+	System.out.print(isActive);
+}
+}
