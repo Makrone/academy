33692f9396d334169781712ee6da62cb201671e9
Makrone <77542250+Makrone@users.noreply.github.com>
2021-01-24 16:01:57 +0300

    Update Test1.java

--- academy/src/by/academy/homework/lesson1/Test1.java
+++ academy/src/by/academy/homework/lesson1/Test1.java
@@ -12,32 +12,32 @@ public class Test1 {
 			System.out.println("Ошибка! Введите число");
 			return;
 		}
-		System.out.print("Введите возрост покупателя: ");
+		System.out.print("Введите возраст : ");
 		int age = sc.nextInt();
 		if (age < 0) {
-			System.out.println("Ошибка! Введите число");
+			System.out.println("Ошибка! Введите возраст");
 			return;
 		}
 
 		if (cost < 100) {
-			System.out.println("Сумма скидки 5% и она составляет: " 
+			System.out.println("Сумма скидки 5% : " 
 								+ ((1 - 0.05) * cost));
 		} else if (cost >= 100 & cost < 200) {
-			System.out.println("Сумма скидки 7% и она составляет: " 
+			System.out.println("Сумма скидки 7% : " 
 								+ ((1 - 0.07) * cost));
 		} else if (cost >= 200 & cost < 300) {
 			if (age > 18) {
-				System.out.println("Сумма скидки 16% и она составляет: " 
+				System.out.println("Сумма скидки 16% : " 
 								+ ((1 - 0.16) * cost));
 			} else {
-				System.out.println("Сумма скидки 9% и она составляет: " 
+				System.out.println("Сумма скидки 9% : " 
 								+ ((1 - 0.09) * cost));
 			}
 		} else if (cost >= 300 & cost < 400){
-			System.out.println("Сумма скидки 15% и она составляет: " 
+			System.out.println("Сумма скидки 15% : " 
 								+ ((1 - 0.15) * cost));
 		} else {
-			System.out.println("Сумма скидки 20% и она составляет: " 
+			System.out.println("Сумма скидки 20% : " 
 								+ ((1 - 0.2) * cost));
 		}
 	}
